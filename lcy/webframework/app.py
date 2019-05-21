d7854b359a9cd78cb53c7dad22ca65960cabf867
breakingdevil <hkcoldmoon@gmail.com>
2019-05-21 02:19:36 -0700

    work done

--- lcy/webframework/app.py
+++ lcy/webframework/app.py
@@ -1,13 +1,36 @@
 #!/usr/bin/env python3
 from flask import *
+import os
+from datetime import datetime
+import base64
+from random import randint
 
+APP_ROOT = os.path.dirname(os.path.abspath(__file__)) + '/static/submit/'
 app = Flask(__name__)
 
 
 # fd == facedetect
+@app.route("/")
+def index():
+    return redirect("/fd")
+
+
 @app.route("/fd", methods=['GET', 'POST'])
 def fd():
     return render_template("js_face_detection.html")
+
+
+@app.route("/submitPic", methods=['GET', 'POST'])
+def submitPic():
+    picBase64 = request.form['data'][23:]
+    if picBase64 is None or picBase64 == "":
+        return "???"
+    with open(APP_ROOT + str(datetime.now())[:10] + str(randint(0,100)) + ".jpg", "wb") as f:
+        f.write(base64.urlsafe_b64decode(picBase64.encode("utf-8")))
+        f.close()
+    return "ok!"
+
+
 
 
 if __name__ == '__main__':
