a226912573d84e270a113296fc2c9aba0f543b5d
iBug <7273074+iBug@users.noreply.github.com>
2019-05-26 22:57:55 +0800

    Cleanup #1

--- lcy/webframework/app.py
+++ lcy/webframework/app.py
@@ -5,18 +5,13 @@ from datetime import datetime
 import base64
 from random import randint
 
-APP_ROOT = os.path.dirname(os.path.abspath(__file__)) + '/static/submit/'
+IDENT_DIR = os.path.dirname(os.path.abspath(__file__)) + '/ident/'
 app = Flask(__name__)
 
 
 # fd == facedetect
 @app.route("/")
 def index():
-    return redirect("/fd")
-
-
-@app.route("/fd", methods=['GET', 'POST'])
-def fd():
     return render_template("js_face_detection.html")
 
 
@@ -24,13 +19,11 @@ def fd():
 def submitPic():
     picBase64 = request.form['data'][23:]
     if picBase64 is None or picBase64 == "":
-        return "???"
-    with open(APP_ROOT + str(datetime.now())[:10] + str(randint(0,100)) + ".jpg", "wb") as f:
+        return "An error occurred when receiving your images."
+    with open(IDENT_DIR + str(datetime.now())[:10] + str(randint(0,100)) + ".jpg", "wb") as f:
         f.write(base64.urlsafe_b64decode(picBase64.encode("utf-8")))
         f.close()
     return "ok!"
-
-
 
 
 if __name__ == '__main__':
